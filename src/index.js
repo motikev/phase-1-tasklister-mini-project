a363cce097faddfe32318c86b57699f888d815fc
motikev <kmotties@gmail.com>
2023-06-21 13:13:34 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskInput = document.querySelector("#new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = taskInput.value;
+    taskList.appendChild(newTask);
+
+    taskInput.value = "";
+  });
 });
